2d31ebd2464e3b96701e9c3dc807294f472cd40f
John Panton <johnpanton29@outlook.com>
2025-03-27 13:22:50 +0000

    Tweaked appearance and updated photo of myself

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import './About.css'
-import personalImage from '../images/Image.jpeg'
+import personalImage from '../images/Image6.jpg'
 import personalImage1 from '../images/Image[1].jpeg'
 import personalImage2 from '../images/Image[2].jpeg'
 import personalImage3 from '../images/Image[3].jpeg'
